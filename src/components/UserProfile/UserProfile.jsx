f6c9489ce3699665baf675461c09813d2078307b
maya <maya.salcedo07@gmail.com>
2021-10-06 08:37:28 +0300

    added error and loading handler

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -8,16 +8,21 @@ import Link from '../Button/Styles';
 export function UserProfile() {
   const { user_id } = useParams();
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const getData = async () => {
     try {
+      setIsLoading(true);
       const { data } = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${user_id}`
       );
-
       setUser(data);
+      setIsLoading(false);
     } catch (err) {
-      console.log(err);
+      setIsError(true);
+      setIsLoading(false);
+      console.error(`Error at UserProfile: ${err}`);
     }
   };
 
@@ -26,27 +31,32 @@ export function UserProfile() {
   }, []);
   return (
     <UserWrapper>
-      <CardWrapper.Box>
-        <CardWrapper.Title>Name: {user.name}</CardWrapper.Title>
-        <CardWrapper.Text>
-          Username: {user?.username}
-          <br />
-          Email: {user?.email} <br />
-          Phone: {user?.phone}
-        </CardWrapper.Text>
-        <CardWrapper.Text>
-          Company: {user?.company?.name} <br />
-          Website: {user?.website} <br />
-        </CardWrapper.Text>
-        <CardWrapper.Text>
-          Address: <br />
-          Street: {user?.address?.street} <br />
-          Suite: {user?.address?.suite}
-          <br />
-          City: {user?.address?.city} <br />
-          Zipcode: {user?.address?.zipcode} <br />
-        </CardWrapper.Text>
-      </CardWrapper.Box>
+      {isError && <h1> ERROR </h1>}
+      {isLoading ? (
+        <h1>LOADING </h1>
+      ) : (
+        <CardWrapper.Box>
+          <CardWrapper.Title>Name: {user.name}</CardWrapper.Title>
+          <CardWrapper.Text>
+            Username: {user?.username}
+            <br />
+            Email: {user?.email} <br />
+            Phone: {user?.phone}
+          </CardWrapper.Text>
+          <CardWrapper.Text>
+            Company: {user?.company?.name} <br />
+            Website: {user?.website} <br />
+          </CardWrapper.Text>
+          <CardWrapper.Text>
+            Address: <br />
+            Street: {user?.address?.street} <br />
+            Suite: {user?.address?.suite}
+            <br />
+            City: {user?.address?.city} <br />
+            Zipcode: {user?.address?.zipcode} <br />
+          </CardWrapper.Text>
+        </CardWrapper.Box>
+      )}
       <Link to="/">Go back</Link>
     </UserWrapper>
   );
